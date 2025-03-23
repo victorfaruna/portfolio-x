b4711a1376d66889d0dce56ad2e762e803953b39
victorfaruna <farunavictor043@gmail.com>
2025-03-23 03:14:20 +0100

    update

--- src/components/HeroBig.tsx
+++ src/components/HeroBig.tsx
@@ -91,7 +91,7 @@ export default function HeroBig() {
           icon="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425
         5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86
         13.028h1.36L4.323 2.145H2.865z"
-          link="https://x.com/dev_victor_f"
+          link="https://x.com/0xfaruna"
           text="X / Twitter"
         />
         <SocialItem
