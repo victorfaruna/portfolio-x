68c763bcd4edbe9b5a032575b1879de93b25831d
victorfaruna <farunavictor043@gmail.com>
2025-04-09 19:57:48 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
+"use client";
 import About from "@/components/About";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import HeroArticles from "@/components/HeroArticles";
 import Projects from "@/components/Projects";
-import React from "react";
+import { Splash } from "@/components/Splash";
+import { cn } from "@/lib/utils";
+import React, { useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="w-screen h-full overflow-x-hidden">
-      <Header />
-      <Hero />
-      <HeroArticles />
-      <About />
-      <Projects />
-    </main>
-  );
+    const [isBooting, setIsBooting] = useState(true);
+    setTimeout(() => {
+        setIsBooting(false);
+    }, 3000);
+    return (
+        <main className="w-screen h-full overflow-x-hidden">
+            <div
+                className={cn(
+                    "w-screen h-screen fixed z-[999] top-0 left-0 bg-black transition-all duration-1000",
+                    isBooting ? "opacity-100" : "opacity-0"
+                )}
+            >
+                <Splash />
+            </div>
+            <Header />
+            <Hero />
+            <HeroArticles />
+            <About />
+            <Projects />
+        </main>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,36 +1,87 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      screens: {
-        "2xl": { max: "1535px" },
-        // => @media (max-width: 1535px) { ... }
-        xl: { max: "1279px" },
-        // => @media (max-width: 1279px) { ... }
-        lg: { max: "1023px" },
-        // => @media (max-width: 1023px) { ... }
-        md: { max: "767px" },
-        // => @media (max-width: 767px) { ... }
-        sm: { max: "639px" },
-        // => @media (max-width: 639px) { ... }
-      },
-      colors: {
-        main: "rgba(var(--color-main), 1)",
-        "color-1": "rgba(var(--color-1), 1)",
-        "color-2": "rgba(var(--color-2), 1)",
-      },
-      fontFamily: {
-        poppins: "var(--font-poppins)",
-        openSans: "var(--font-open-sans)",
-        firaCode: "var(--font-fira-code)",
-      },
-    },
+  	extend: {
+  		screens: {
+  			'2xl': {
+  				max: '1535px'
+  			},
+  			xl: {
+  				max: '1279px'
+  			},
+  			lg: {
+  				max: '1023px'
+  			},
+  			md: {
+  				max: '767px'
+  			},
+  			sm: {
+  				max: '639px'
+  			}
+  		},
+  		colors: {
+  			main: 'rgba(var(--color-main), 1)',
+  			'color-1': 'rgba(var(--color-1), 1)',
+  			'color-2': 'rgba(var(--color-2), 1)',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			poppins: 'var(--font-poppins)',
+  			openSans: 'var(--font-open-sans)',
+  			firaCode: 'var(--font-fira-code)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
